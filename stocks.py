6a5ae4812bdca2ee55cc52e420d3eda85f9fd764
CrungeyDownloadsViruses <76408831+CrungeyDownloadsViruses@users.noreply.github.com>
2023-03-09 12:49:39 -0500

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -1,15 +1,21 @@
-from yahoo_fin import stock_info as si
+import yfinance as yf
 from art import *
 import threading
 
-previous = si.get_live_price("NVDA")
+previous = yf.Ticker("NVDA").history(period="1d")["Close"].iloc[-1]
 oldprice = 0
 
 def printit():
     global oldprice
     threading.Timer(2.0, printit).start()
-    price = si.get_live_price("NVDA")
-    value = (price - 235.0) * 42
+    price = yf.Ticker("NVDA").history(period="1d")["Close"].iloc[-1]
+    price1 = yf.Ticker("UBI.PA").history(period="1d")["Close"].iloc[-1]
+    price2 = yf.Ticker("CHWY").history(period="1d")["Close"].iloc[-1]
+    price3 = yf.Ticker("MSFT").history(period="1d")["Close"].iloc[-1]
+    price4 = yf.Ticker("SONY").history(period="1d")["Close"].iloc[-1]
+    value = ((price - 235.0) * 22) + (price1 - 21.68) + (price2 - 41.29) + (price3 - 267.32) + (price4 - 88.25)
+    percents = "NVDA: " + str((price - 235.0) * 22) + "$ UBI.PA: " + str(price1 - 21.68) + "$ CHWY: " + str(price2 -41.29) + "$ MSFT: " + str(price3 - 267.32) + "$ SONY: " + str(price4 - 88.25) + "$"
+    print (percents)
     
     if(value > oldprice):
         print("                                                                                                                  ##%&&&          ")
@@ -75,6 +81,7 @@ def printit():
         print("     ......                          .,                                                           &(((((((((((#          ") 
         print("      ....                           ..,                                                            (((/(//(/(,          ") 
         print("        .                   .       ..                                                           (////////////           ")
+
         tprint(f"${value:.2f}" + "\\" + "/",font="varsity")
     
         
